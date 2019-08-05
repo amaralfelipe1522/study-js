83f328b6658078d49ff82de2304360208bd03fcc
amaralfelipe1522 <amaral.felipe@live.com>
2019-08-05 20:38:36 -0300

    Add examples of DOM

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function importar (){
-    console.log("importou")
+    console.log("importou");
+};
+importar();
+
+var createButton = function(){
+    var button = document.createElement('button');
+    button.innerHTML = 'Create Button';
+    let i = 0;
+    button.onclick = function(){
+    //    alert('Alertou');
+    //    return false;
+    createButton();
+    };
+    document.body.appendChild(button);
 };
 
-importar()
+
+
