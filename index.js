baeb3d7885acce4d15325e4689e6dfb10739d05e
Felipe Amaral de Souza <felipe.asouza@ultra.corp>
2019-08-06 14:55:13 -0300

    Add: creating UL/LI dynamically

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ function importar (){
 };
 importar();
 
-var createButton = function(){
-    var button = document.createElement('button');
-    button.innerHTML = 'Create Button';
-    let i = 0;
-    button.onclick = function(){
-    //    alert('Alertou');
-    //    return false;
-    createButton();
-    };
-    document.body.appendChild(button);
+// (function createButton(){
+//     var button = document.createElement('button');
+//     button.innerHTML = 'Create Button';
+//     button.onclick = function(){
+//     createButton();
+//     };
+//     document.body.appendChild(button);
+// })();
+
+// function commit() {
+//     let value = document.getElementById("testiculo").value;
+//     document.getElementById("resultID").innerHTML = value;
+// };
+
+function createUl () {
+    var ul = document.createElement("ul");
+    var i = 0;
+    ul.setAttribute("id","idUl");
+    document.body.appendChild(ul);
+    console.log(ul);
+    function commit() {
+        let valor = document.getElementById("testiculo").value;
+        document.getElementById("testiculo").value = '';
+        i += 1;
+        var li = document.createElement("li");
+        li.setAttribute ("id",`idLi${i}`);
+        ul.appendChild(li);
+        document.getElementById(`idLi${i}`).innerHTML = valor;
+        console.log(`idLi${i}`);
+    }
+    return [commit]
 };
 
-
-
+let f = createUl();
+let commit = f[0];
