dc6b01dacadc086669ce8a60c03fc2ec9dce4dfb
amaralfelipe1522 <amaral.felipe@live.com>
2019-08-25 12:45:46 -0300

    Add NodeJS project

--- NodeStudy/server.js
+++ NodeStudy/server.js
@@ -1,15 +1,33 @@
-const http = require('http');
-const servidor = http.createServer(function (req, resp){
-    resp.end(`
-        <html>
-            <head>
-                <meta charset="utf-8">
-            </head>
-            <body>
-                <h1> Casa do Código </h1>
-            </body> 
-        </html>
-    `);
-});
-servidor.listen(3000);
+const express = require('express');
+const app = express();
+app.listen(3000, function(){console.log('Express funfando')});
 
+app.get ('/', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Casa do Código </h1>
+                </body> 
+            </html>
+        `
+    );
+});
+
+app.get ('/livros', function(req, resp) {
+    resp.send(
+        `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Catálogo de Livros </h1>
+                </body> 
+            </html>
+        `
+    );
+});
